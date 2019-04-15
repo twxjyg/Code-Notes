5049323c79eeb8ebfed02aa1723d46ec667e2828
twxjyg <twxjyg@126.com>
2019-04-16 00:11:16 +0800

    add iterate_tree_dfs

---
+++ src/iterate_tree_dfs.cpp
@@ -0,0 +1,96 @@
+#include <memory>
+#include <vector>
+#include <deque>
+#include <iostream>
+
+template <class T>
+struct Node
+{
+    Node(T data) : data{data} {}
+    T data;
+    std::vector<std::shared_ptr<Node<T>>> childs;
+};
+
+std::shared_ptr<Node<int>> BuildANode(int data)
+{
+    return std::make_shared<Node<int>>(data);
+}
+std::shared_ptr<Node<int>> BuildATree()
+{
+    std::shared_ptr<Node<int>> root = BuildANode(0);
+    root->childs.push_back(BuildANode(1));
+    root->childs.push_back(BuildANode(2));
+    root->childs.push_back(BuildANode(3));
+    std::shared_ptr<Node<int>> item = root->childs[2];
+    item->childs.push_back(BuildANode(11));
+    item->childs.push_back(BuildANode(12));
+    item = item->childs[1];
+    item->childs.push_back(BuildANode(22));
+    item->childs.push_back(BuildANode(21));
+    item->childs.push_back(BuildANode(22));
+    return root;
+}
+int main(int argc, char *argv[])
+{
+    auto tree = BuildATree();
+    std::vector<std::shared_ptr<Node<int>>> left;
+    std::vector<std::shared_ptr<Node<int>>> curr_path;
+    auto iter = tree;
+    curr_path.push_back(iter);
+    while (true)
+    {
+        if (!iter->childs.empty())
+        {
+            curr_path.push_back(iter->childs.front());
+            if (iter->childs.size() > 1)
+            {
+                left.insert(left.end(), iter->childs.begin() + 1, iter->childs.end());
+            }
+            iter = iter->childs.front();
+        }
+        else
+        {
+            // std::cout << "current path:" << std::endl;
+            for (auto point : curr_path)
+            {
+                std::cout << point->data << "->";
+            }
+            std::cout << "*" << std::endl;
+            std::cout << std::endl;
+            // std::cout << "current left:" << std::endl;
+            // for (auto point : left)
+            // {
+            //     std::cout << point->data << ",";
+            // }
+            // std::cout << std::endl;
+            bool has = false;
+            while (!has)
+            {
+                curr_path.pop_back();
+                if (!curr_path.empty())
+                {
+                    for (auto point : curr_path.back()->childs)
+                    {
+                        if (!left.empty() && point == left.back())
+                        {
+                            curr_path.push_back(left.back());
+                            iter = left.back();
+                            left.pop_back();
+                            has = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        if (left.empty() && curr_path.empty())
+        {
+            break;
+        }
+    }
+    return 0;
+}
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -15,14 +15,31 @@ class Matrix
             }
         }
     }
+    template <std::size_t other_rows, std::size_t other_cols, std::size_t new_rows, std::size_t new_cols>
+    Matrix<T, new_rows, new_cols> dot(const Matrix<T, other_rows, other_cols> &other)
+    {
+        Matrix<T, rows, other_cols> data(0.0);
+        for (unsigned int row_index = 0; row_index < rows; row_index++)
+        {
+            for (unsigned int col_index = 0; col_index < other_cols; col_index++)
+            {
+                for (unsigned int index = 0; index < cols; index++)
+                {
+                    data[row_index][col_index] += data_[row_index][index] * other[index][col_index];
+                }
+            }
+        }
+        return data;
+    }
 
   private:
     std::array<std::array<T, cols>, rows> data_;
 };
+
 int main(int argc, char const *argv[])
 {
     Matrix<double, 3, 3> a(0), b(1);
-    auto c = a * b;
+    auto c = a.dot(b);
     std::cout << c << std::endl;
     return 0;
 }
